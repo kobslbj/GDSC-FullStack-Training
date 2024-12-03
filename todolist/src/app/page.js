c78f7f84d14d189bf003bacc39e7b2ae95d5665e
SG <97837305+S-Garry@users.noreply.github.com>
2024-12-03 14:30:47 +0800

    feat: add todo item component

--- todolist/src/app/page.js
+++ todolist/src/app/page.js
@@ -3,20 +3,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
+import Todoitem from "@/components/Todoitem";
 
 export default function Home() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-  const addTodo = () => setTodos([
-    ...todos,
-    {
-      id: todos.length + 1,
-      name: todos.length + 1,
-    }, 
-  ])
+  const addTodo = () => {
+    var id = todos.length ? ((todos.at(-1)).id + 1) : 0;
+    var name = `This is todo name ${id}`;
+
+    var newTodo = {
+      id: id,
+      name: name
+    };
+
+    return setTodos([
+      ...todos,
+      newTodo
+    ]);
+  };
 
   return (
-    <>
+    <body>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <div className={styles.appbar}>
         <div className={`${styles.font} ${styles.title} ${styles.user}`}>Hi Justin~</div>
@@ -29,7 +37,7 @@ export default function Home() {
             <ul id="ul">
               {todos.map((todo) => (
                 <li key={todo.id}>
-                  <div>{todo.name}</div>
+                  <Todoitem todo={todo} todos={todos} callback={setTodos} />
                 </li>
               ))}
             </ul>
@@ -40,6 +48,6 @@ export default function Home() {
           </button>
         </div>
       </div>
-    </>
+    </body>
   );
 }
--- todolist/src/components/Todoitem.jsx
+++ todolist/src/components/Todoitem.jsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
+import styles from "@/app/page.module.css";
 
-export default function Todoitem() {
-    
+export default function Todoitem({todo, todos, callback}) {
+    const [item, setItem] = useState(todo)
+
+
+    const deleteItem = () => {
+        const updateTodos = todos.filter(item => item.id !== todo.id);
+        return callback(updateTodos);
+    }
+
+    return (
+        <div className={styles.mission}>
+            <input type="checkbox" />
+            <div className={`${styles.font} ${styles.subtitle} ${styles.name}`}>{item.name}</div>
+            <button className={styles.delete} onClick={deleteItem}><i className="fa fa-trash"></i></button>
+        </div>
+    );
 }
