b5989a4630fc220cb337d49b0fed1b5c47a71e9a
SG <97837305+S-Garry@users.noreply.github.com>
2024-12-16 23:47:50 +0800

    fix: Code modified

--- todolist/src/components/Todoitem.jsx
+++ todolist/src/components/Todoitem.jsx
@@ -14,7 +14,8 @@ export default function Todoitem({todo, todos, callback}) {
         <div className={styles.mission}>
             <input type="checkbox" />
             <div className={`${styles.font} ${styles.subtitle} ${styles.name}`}>{item.name}</div>
-            <button className={styles.delete} onClick={deleteItem}><i className="fa fa-trash"></i></button>
+            {/* <button className={styles.delete} onClick={deleteItem}><i className="fa fa-trash"></i></button> */}
+            <i className={`fa fa-trash ${styles.delete}`} onClick={deleteItem} />
         </div>
     );
 }
