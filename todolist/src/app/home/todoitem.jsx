ea4bf4e890c116a8cc4df87488d99bd227f3f3f6
SG <97837305+S-Garry@users.noreply.github.com>
2024-12-26 12:37:15 +0800

    fix: checkbox change error

--- todolist/src/app/home/todoitem.jsx
+++ todolist/src/app/home/todoitem.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import globalstyles from "@/app/page.module.css";
 import styles from "./page.module.css";
 
-export default function Todoitem({todo, todos, callback}) {
+export default function Todoitem({ todo, todos, callback }) {
     const [item, setItem] = useState(todo)
     const [isComplete, setComplete] = useState(false)
 
+    // Update the state when isComplete changes
+    const handleCheckboxChange = () => {
+        setComplete(prevState => !prevState);
+    };
 
     const deleteItem = () => {
         const updateTodos = todos.filter(item => item.id !== todo.id);
@@ -14,7 +18,7 @@ export default function Todoitem({todo, todos, callback}) {
 
     return (
         <div className={styles.mission}>
-            <input type="checkbox" checked={isComplete}/>
+            <input type="checkbox" checked={isComplete} onChange={handleCheckboxChange} />
             <div className={`${globalstyles.font} ${globalstyles.subtitle} ${styles.name}`}>{item.name}</div>
             {/* <button className={styles.delete} onClick={deleteItem}><i className="fa fa-trash"></i></button> */}
             <i className={`fa fa-trash ${styles.delete}`} onClick={deleteItem} />
